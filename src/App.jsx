a84b78ee04c3c2fe8b37a45dc6d30e5bccb0ed58
Ranahansa <CODSE213F-053@student.nibm.lk>
2023-12-16 19:04:57 +0530

    commit 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState } from 'react'
 import './App.css'
+import ToggleButton from './components/ToggleButton'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div>
+
+      <ToggleButton />
       
     </div>
   )
