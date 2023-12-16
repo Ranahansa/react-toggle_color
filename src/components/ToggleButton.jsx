73cb050f9777fa641bc460da74413f7c8bdef527
Ranahansa <CODSE213F-053@student.nibm.lk>
2023-12-16 23:00:53 +0530

    commit 3

--- src/components/ToggleButton.jsx
+++ src/components/ToggleButton.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './style.css'
 
-const ToggleButton = () => {
+const ToggleButton = () =>{
+    const [backgroundColor, setBackgroundColor] = useState('white')
+        const[textColor, setTextColor] = useState('#1b1b1b')
+        const[buttonStyle, setButtonStyle] = useState('white')
+        const[paragraphStyle, setParagraphStyle] = useState('white')
+    
+    const handleClick = () =>{
+        setBackgroundColor(backgroundColor === 'white' ? '#1b1b1b' : 'white')
+        setTextColor(textColor === '#1b1b1b' ? '#ffa31a' : '#1b1b1b')
+        setButtonStyle(backgroundColor === 'white' ? '#1b1b1b' : 'white')
+        setParagraphStyle(backgroundColor === '#1b1b1b' ? "Light" : "Dark")
+    }
     return (
         <div>
-            
+            <section style={{
+                backgroundColor,
+                color: textColor
+            }}>
+                <button onClick={handleClick} style={{
+                    backgroundColor : buttonStyle,
+                    color: textColor,
+                    border: '2px solid ${textColor}',
+                }}>{paragraphStyle} Theme</button>
+
+
+            <section className='content'>
+                <h1>
+                    Welcome To The {paragraphStyle} Side !
+                </h1>
+            </section>
+
+            </section>
         </div>
     )
 }
